2962f20418c5bc6f0a9016df9dfb0e99c913f4bf
Louis Chen <louistjchen@gmail.com>
2017-10-18 22:48:03 -0400

    checked in lab2 starter files

---
+++ Starter1/test.c
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+
+{
+
+/**** yolo this is comment please don't scan me in.   * *? */
+	const int a = +2.3;
+	a = b + c;
+	float hheheh = +0.20;
+	float whwh = -20.23;
+	return 0;
+// wahahawhahahgeoih ???
+
+	L = 0;
+}
+
+
+// a.bEC
+// a max
+// 0.00001
+// 2.000001
